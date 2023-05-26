ea36dc57850a87368c75bc2c87272ef339fede61
mojafa <jmohsa@gmail.com>
2023-05-26 09:44:16 +0300

    a few stuff

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -2,8 +2,9 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class RandomizerPage extends StatefulWidget {
+class RandomizerPage extends HookWidget {
   final int min, max;
   const RandomizerPage({
     Key? key,
