93340151baa51b8503d2b45766a7d442bfeee586
mojafa <jmohsa@gmail.com>
2023-05-26 09:43:47 +0300

    removed const from the randompage

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class AppWidget extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const RangeSelectorPage(),
+      home: RangeSelectorPage(),
     );
   } 
 }
