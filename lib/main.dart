637a8b3ccce6861b9ea9d093f6c44f4ec91b75dc
mojafa <jmohsa@gmail.com>
2023-05-27 08:23:39 +0300

    wrapped everything in a ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_foundations/randomizer_change_notifier.dart';
 import 'package:flutter_foundations/range_selector_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const AppWidget());
@@ -12,6 +14,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) { 
     return ChangeNotifierProvider(
+      create: (context) => RandomizerChangeNotifier(),
       child: MaterialApp(
         title: 'Random number generator',
         theme: ThemeData(
