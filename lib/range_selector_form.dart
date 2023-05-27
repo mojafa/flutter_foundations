96964f8b28ea9ec0426dc3b6796094cbcfb797a3
mojafa <jmohsa@gmail.com>
2023-05-27 09:50:47 +0300

    using statenotifier instead of change notifier

--- lib/range_selector_form.dart
+++ lib/range_selector_form.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_foundations/randomizer_change_notifier.dart';
+import 'package:flutter_foundations/main.dart';
 import 'package:flutter_foundations/range_selector_page.dart';
-import 'package:provider/provider.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 typedef IntValueSetter = void Function(int? value);
 
-class RangeSelectorForm extends StatelessWidget{
+class RangeSelectorForm extends ConsumerWidget{
   const RangeSelectorForm({
     Key? key,
     required this.formKey,
@@ -16,7 +15,7 @@ class RangeSelectorForm extends StatelessWidget{
 
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Form(
         key: formKey,
         child: Padding( 
@@ -26,12 +25,12 @@ class RangeSelectorForm extends StatelessWidget{
             children: [
               RangeSelectorTextFormField(
                 labelText: 'Minimum',
-                intValueSetter: (value) => context.read<RandomizerChangeNotifier>().min = value ?? 0,
+                intValueSetter: (value) => ref.read(randomizerProvider.notifier).setMin(value!),
               ),
               const SizedBox(height: 16),
               RangeSelectorTextFormField(
                 labelText: 'Maximum',
-                intValueSetter: (value) => context.read<RandomizerChangeNotifier>().max = value ?? 0,
+                intValueSetter: (value) => ref.read(randomizerProvider.notifier).setMax(value!),
               ),
             ],
           ),
